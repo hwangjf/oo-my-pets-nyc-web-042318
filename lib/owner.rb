a5057f27c4d856bb5731d13498cb80966bc13f41
github username <jeffreyhwang90@gmail.com>
2018-04-30 23:39:50 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,73 @@
+require 'pry'
 class Owner
-  # code goes here
+
+  attr_accessor :pets, :name
+  attr_reader :species
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def buy_cat(name)
+    name = Cat.new(name)
+    @pets[:cats] << name
+  end
+
+  def buy_dog(name)
+    name = Dog.new(name)
+    @pets[:dogs] << name
+  end
+
+  def buy_fish(name)
+    name = Fish.new(name)
+    @pets[:fishes] << name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def walk_dogs
+    pets[:dogs].map {|dog| dog.mood = "happy"}
+  end
+
+  def play_with_cats
+    pets[:cats].map {|cat| cat.mood = "happy"}
+  end
+
+  def feed_fish
+    pets[:fishes].map {|fish| fish.mood = "happy"}
+  end
+
+  def sell_pets
+    pets.each do |type, pets|
+      pets.map {|pet| pet.mood = "nervous"}
+    end
+    pets.clear
+  end
+
+  def list_pets
+    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
+  end
 end
+#
+# When an owner plays with a cat or feeds a fish or walks a dog, that pet will get happier.
+# Remember that the pets method stores all of an owners pets.
+# The @pets hash stored in that method is full of instances of the Cat/Dog/Fish class.
+# That means you can call Cat/Dog/Fish instance methods (such as .mood=) on those pets.
